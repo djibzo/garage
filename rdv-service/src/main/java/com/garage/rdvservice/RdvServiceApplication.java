f6cc5d338090c695ce19164acc5b4ea561932381
FallDjibril <djibril.fall2@unchk.edu.sn>
2025-06-15 17:13:14 +0000

    Ajout du service discovery

--- rdv-service/src/main/java/com/garage/rdvservice/RdvServiceApplication.java
+++ rdv-service/src/main/java/com/garage/rdvservice/RdvServiceApplication.java
@@ -2,8 +2,10 @@ package com.garage.rdvservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RdvServiceApplication {
 
     public static void main(String[] args) {
